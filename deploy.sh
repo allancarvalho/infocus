2fa3581654230936605b3be1d709f4db79ad484c
allancarvalho <allan_tk@msn.com>
2014-11-17 23:27:48 -0200

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 echo "Executing push on github";
-cd application/config/;
-cp database.php database.php.temp;
-cp database.phpprod database.php;
-git commit -m "autodeploy";
-cd ../..;
+#cd application/config/;
+#cp database.php database.php.temp;
+#cp database.phpprod database.php;
+#git commit -m "autodeploy";
+#cd ../..;
 git push; 
 
 echo "Executing deploy on FTP";
 git ftp push;
 
-cd application/config/;
-mv database.php.temp database.php;
+#cd application/config/;
+#mv database.php.temp database.php;
 
-cd ../..;
+#cd ../..;
 
 echo "deploy ended";
 
